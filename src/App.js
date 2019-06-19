5eca2da27d25e81423cc51750496a00bc7ac8348
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-18 20:55:50 -0400

    Update App.js
    Create 3 <Route /> setting their path prop equal to /, /about, /contact with their respective components.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { Home, About, Contact, Navigation } from './components';
 const App = () => (
   <div>
     <Navigation />
+    
+    <Route path="/" exact component={Home} />
+    <Route path="/about" exact component={About} />
+    <Route path="/contact" exact component={Contact} />
   </div>
 );
 
